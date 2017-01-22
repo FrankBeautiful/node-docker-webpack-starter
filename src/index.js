0a231f0834ea91d30ce89e8ea6c5d6ee7e3dd8f7
Bruce MacFarlane <bruce_macfarlane@apple.com>
2017-01-21 18:09:55 -0800

    minor

--- src/index.js
+++ src/index.js
@@ -13,5 +13,3 @@ const testText = "Change text here to check live reload when saved.";
 /* ------------------------------------------- */
 
 document.getElementById("livereloadfeedback").innerHTML = testText;
-
-console.log("here");
